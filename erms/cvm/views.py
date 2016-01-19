68b8678aad5ba0c055aaa675b2fb82f1a9de0063
Shehan <shehana@hsenidmobile.com>
2016-01-19 14:55:52 +0530

    Validation added

--- erms/cvm/views.py
+++ erms/cvm/views.py
@@ -2,7 +2,28 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 
-
-@login_required
 def addCVs(request):
-    return render_to_response("erms/add_cv_form.html", context_instance=RequestContext(request))
+    page_parameters = {}
+    if request.method == "GET":
+        if request.GET.get('firstName') and request.GET.get('secondName'):
+            start_date = request.GET['firstName']
+            end_date = request.GET['secondName']
+            init_request = "false"
+            print("#")
+            print("#")
+            print(start_date)
+            print(end_date)
+            print("#")
+            print("#")
+        else:
+            init_request = "true"
+        page_parameters = {"init_request": init_request}
+    return render_to_response("erms/add_cv_form.html", page_parameters, context_instance=RequestContext(request))
+
+
+
+
+
+# @login_required
+# def addCVs(request):
+#     return render_to_response("erms/add_cv_form.html", context_instance=RequestContext(request))
