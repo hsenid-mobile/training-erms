be30add48913bd471f2f41ab4cca4c891f293573
JayKrish <jayakrishnanr@hsenidmobile.com>
2016-03-08 00:24:10 +0530

    changed database to postgres

--- test1/test1/settings.py
+++ test1/test1/settings.py
@@ -77,8 +77,11 @@ WSGI_APPLICATION = 'test1.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+       'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'test1',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': '127.0.0.1',
     }
 }
 
